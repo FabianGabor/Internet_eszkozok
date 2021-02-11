ac009ef158dd4cba4a3cfa43f41752c6eb3d68ad
Fábián Gábor <info@fabiangabor.com>
2021-02-11 11:23:05 +0100

    echo date time

---
+++ 1/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+
+<body>
+    <?php
+        echo date("Y.M.d. H:m:s");
+    ?>
+</body>
+</html>
