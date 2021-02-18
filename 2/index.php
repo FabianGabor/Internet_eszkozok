b02805bf0ad89e81aea60eec66bf927809d0d3d4
Fábián Gábor <info@fabiangabor.com>
2021-02-18 10:50:30 +0100

    Revert "gyak2"
    This reverts commit e36266a9e3d49cd0e1c2d6d47e5498194aa96152.

---
+++ 2/index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <title>Portal</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/css/foundation.min.css" integrity="sha256-ogmFxjqiTMnZhxCqVmcqTvjfe1Y/ec4WaRj/aQPvn+I=" crossorigin="anonymous">
+    -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/combine/npm/foundation-sites@6.6.3/dist/css/foundation.min.css,npm/foundation-sites@6.6.3/dist/css/foundation-float.min.css,npm/foundation-sites@6.6.3/dist/css/foundation-prototype.min.css">
+    <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+<p>Hello world! This is HTML5 Boilerplate.</p>
+<?php
+/**
+ * include / require
+ */
+include('register.php');
+?>
+
+
+<!--
+<script src="js/app.js"></script>
+-->
+<script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/js/foundation.min.js" integrity="sha256-pRF3zifJRA9jXGv++b06qwtSqX1byFQOLjqa2PTEb2o=" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
